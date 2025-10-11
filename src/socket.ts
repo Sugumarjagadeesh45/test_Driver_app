ba79bb900fc32c6b72bdb2680a3070de1ed6e118
Sugumarjagadeesh45 <sugumarjagadeesh.com>
2025-10-11 16:32:31 +0530

    some update

--- src/socket.ts
+++ src/socket.ts
@@ -5,7 +5,7 @@ import { Alert } from "react-native";
 
 
 
-const socket = io("https://1fa4634fb106.ngrok-free.app", { // Android emulator
+const socket = io("https://0b99d73ef6b3.ngrok-free.app", { // Android emulator
  transports: ["polling", "websocket"], 
   autoConnect: false,
   reconnection: true,
